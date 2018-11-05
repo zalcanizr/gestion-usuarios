551e22475a90f58752694d112ee0b8625a1e8b4a
zalcanizr <39500802+zalcanizr@users.noreply.github.com>
2018-11-05 13:35:51 +0100

    Update UsuarioDTO.java

--- src/main/java/com/cice/gestionusuarios/web/dto/UsuarioDTO.java
+++ src/main/java/com/cice/gestionusuarios/web/dto/UsuarioDTO.java
@@ -22,4 +22,30 @@ public class UsuarioDTO {
         this.user=user;
         this.email=email;
     }
+    public Long getId(){
+        return id;
+    }
+    public void setId(Long id){
+        this.id=id;
+    }
+    public String getNombre(){
+        return nombre;
+    }
+    public void setNombre(String nombre){
+        this.nombre=nombre;
+    }
+    
+    public String getUser(){
+        return user;
+    }
+    public void setUser(String user){
+        this.user=user;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email=email;
+    }
+    
 }
